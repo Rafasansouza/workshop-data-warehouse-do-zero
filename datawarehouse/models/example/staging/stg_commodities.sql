d56b8f953ed7bfc2385c3b016ce3ba88b70d6af1
Rafael Souza <rafasansou1@gmail.com>
2025-05-02 00:14:17 -0300

    adicionando datamart e staging

---
+++ datawarehouse/models/example/staging/stg_commodities.sql
@@ -0,0 +1,25 @@
+-- import
+
+with source as (
+    select
+        "Date",
+        "Close",
+        simbolo
+    from
+        {{ source ('dbsales', 'commodities')}}
+),
+
+-- renamed
+
+renamed as (
+    select
+        cast("Date" as date) as data,
+        "Close" as valor_fechamento,
+        simbolo
+    from
+        source
+)
+
+-- query
+
+select * from renamed
